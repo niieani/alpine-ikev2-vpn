f7d9dae85349ac367b8ddd97804a19307585e8fa
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2018-05-04 23:11:37 +0200

    better iptables

--- init.sh
+++ init.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 set -e
 # Preload iptables,Rule lost when preventing restart of container!
-if [ -f /etc/sysconfig/iptables ]
-then
-  :
-  # iptables-restore < /etc/sysconfig/iptables
-fi
+
+# required for VPN connection:
+iptables -t mangle -A FORWARD -s 10.28.0.0/24 -o eno1 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+
+# internet connectivity:
+iptables -t nat -A POSTROUTING -s 10.28.0.0/24 -o eno1 -j MASQUERADE
+
+# port forward:
+iptables -t nat -A PREROUTING -p tcp --dport 40000 -j DNAT --to 10.28.0.80
+iptables -t nat -A PREROUTING -p tcp --dport 40001 -j DNAT --to 10.28.0.80
+
+# if [ -f /etc/sysconfig/iptables ]
+# then
+#   iptables-restore -n < /etc/sysconfig/iptables
+# fi
 
 # Repair gcp container restart, can not access google family bucket(Disable pmtu discovery!)
 sysctl -w net.ipv4.ip_forward=1
